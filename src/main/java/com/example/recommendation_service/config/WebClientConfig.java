ef3e68d6f295c7b75e3df53eaa44795b098dbeec
201912025 <michi2012@naver.com>
2025-09-19 18:05:16 +0900

    feat: webclient 타임아웃 추가

--- src/main/java/com/example/recommendation_service/config/WebClientConfig.java
+++ src/main/java/com/example/recommendation_service/config/WebClientConfig.java
@@ -2,12 +2,20 @@ package com.example.recommendation_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
+
     @Bean
     public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+        HttpClient httpClient = HttpClient.create()
+                                          .responseTimeout(Duration.ofSeconds(5));
+
+        return WebClient.builder()
+                        .clientConnector(new ReactorClientHttpConnector(httpClient));
     }
 }
