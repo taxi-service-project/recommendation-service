20feab17a9e9db95ca086f045aeb1b3f508ce90b
201912025 <michi2012@naver.com>
2026-01-16 13:12:42 +0900

    feat: 스레드 전환 구간에서 확실한 전파 위해 Hooks 설정 명시적으로 추가

--- src/main/java/com/example/recommendation_service/RecommendationServiceApplication.java
+++ src/main/java/com/example/recommendation_service/RecommendationServiceApplication.java
@@ -2,11 +2,14 @@ package com.example.recommendation_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class RecommendationServiceApplication {
 
 	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
+
 		SpringApplication.run(RecommendationServiceApplication.class, args);
 	}
 
